16bf93d6f4d838a5acae39c16ab236e5ed02bc2f
Sharath-Kumar-R2007 <sharathgowda7944@gmail.com>
2026-03-04 20:41:03 +0530

    day-03-lists-tuples.py
    This program demonstrates basic Python operations using lists and tuples.
    It includes removing elements from a list, working with tuple methods such as index() and count(), and collecting user input to store values dynamically in a list using the append() method.

---
+++ day-03-lists-tuples.py
@@ -0,0 +1,24 @@
+li=[3,4,2,7,9,5,2]
+li.remove(9)
+print(li)
+
+tup=(1,)
+print(tup)
+print(type(tup))
+
+tup=(1,2,3,4)
+print(tup.index(3))
+#print(tup)
+
+tup=(1,2,3,4,4)
+print(tup.count(4))
+
+#Append
+movie=[]
+movie1=input("Enter the first movie name:")
+movie2=input("Enter the second movie name:")
+movie3=input("Enter the 3rd movie name:")
+movie.append(movie1)
+movie.append(movie2)
+movie.append(movie3)
+print(movie)
