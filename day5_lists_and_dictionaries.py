1d3836da32faa140e717258c80c8f8e44653827f
Sharath-Kumar-R2007 <sharathgowda7944@gmail.com>
2026-03-06 22:13:24 +0530

    day5_lists_and_dictionaries.py
    Day 5 – Implemented list count(), sort() and basic dictionary structure in Python.

---
+++ day5_lists_and_dictionaries.py
@@ -0,0 +1,25 @@
+#Count
+grade=["C","M","G","A","A","B","R"]
+print(grade.count("A"))
+
+#sort
+grade=["C","M","G","A","B","R"]
+grade.sort()
+print(grade)
+
+#Dictionary
+dict={
+  "name":"sharath",
+  "cgpa":9.6,
+  "marks":[68,97,95]
+}
+print(dict)
+
+#dectionary
+#Dict
+dictionar = {
+      "cat": "a small animal",
+    "table": ["a piece of furniture","list of facts and figures"]
+}
+print(dictionar)
+
