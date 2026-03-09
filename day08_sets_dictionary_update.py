3d00cb4ff13b61679594bd4c221be595b768d755
Sharath-Kumar-R2007 <sharathgowda7944@gmail.com>
2026-03-09 20:24:09 +0530

    Create day08_sets_dictionary_update.py
    Day 8: Practiced Python sets and dictionary update method.
    Learned how sets remove duplicate values and how to add elements using set methods.

---
+++ day08_sets_dictionary_update.py
@@ -0,0 +1,30 @@
+#update
+new_dect={"name":"Luffy","bounty":"three billions"}
+student.update(new_dect)
+print(student)
+
+#Sets
+my_set={1,2,3,2,1,5,"hellow","hellow","world",7}
+print(set)
+print(type(my_set))
+print(len(my_set))
+
+#empty sets
+empty_sets=set()
+print(type(empty_sets))
+print(empty_sets)
+
+#sets (len)
+subjects={
+"python","java","c++","python","javascript","java","python","java","c++","c"
+}
+print(subjects)
+print(len(subjects))
+
+#Sets method (Add)
+collection=set()
+collection.add(1)
+collection.add(2)
+collection.add(3)
+collection.add(2)
+print(collection)
