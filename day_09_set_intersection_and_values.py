b1613b1e6aed37060174271291b33e842d27cc89
Sharath-Kumar-R2007 <sharathgowda7944@gmail.com>
2026-03-11 19:01:03 +0530

    Create day_10_set_intersection_and_values.py
    Day 10 of my Python learning journey. Practiced set intersection() to find common elements between sets and experimented with storing different data types like integers and strings inside a set.

---
+++ day_09_set_intersection_and_values.py
@@ -0,0 +1,18 @@
+#intersection
+set1 = {1,2,3,4,6,5,7}
+set2 = {0,9,8,7,6,5,4}
+print(set1)
+print(set2)
+print(set1.intersection(set2))
+
+#To store the 9 & 9.0 as separate value
+Value={9,"9.0"}
+print(Value)
+
+#or
+
+Value={
+  ("float",9.0),
+  ("int",9)
+}
+print(Value)
