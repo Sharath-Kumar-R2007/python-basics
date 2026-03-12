feb7fd1e3e21b80e07459f88c0af49c7f104d6eb
Sharath-Kumar-R2007 <sharathgowda7944@gmail.com>
2026-03-12 17:50:21 +0530

    Create day_11_python_while_loops_practice.py
    Day 11: Practiced Python while loops with counting, reverse counting, and iteration examples

---
+++ day_11_python_while_loops_practice.py
@@ -0,0 +1,22 @@
+#LOOPS
+#while loops
+while True:             #infinity
+  print("hello bhai")
+
+#To print 5 times
+count=1
+while count <= 5:
+  print("Hello bro")
+  count += 1
+
+#To print 10000
+i=1
+while i <= 10000:
+  print("Sharath Kumar R",i)
+  i = i+1
+
+# print in Reverse
+count = 10
+while count >= 1:
+  print(count)
+  count -= 1
