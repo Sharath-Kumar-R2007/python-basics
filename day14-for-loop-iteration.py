701cd8016f5eaf3117020eeab108a11d2627cf9d
Sharath kumar R <sharathgowda7944@gmail.com>
2026-03-15 15:01:58 +0530

    Create day14-for-loop-iteration.py
    Day 14 Python practice: using for loops to iterate through different data structures including lists, tuples, and strings. This exercise focuses on understanding how iteration works in Python and how loops can process sequences element by element.

---
+++ day14-for-loop-iteration.py
@@ -0,0 +1,19 @@
+#FOR LOOP
+nums = [1,2,3,4,5,6,7,8]
+for val in nums:
+   print(val)
+
+#FOR LOOP IN LIST
+veggies = ["potato","brijal","ladyfinger","cucumber"]
+for veg in veggies:
+   print(veg)
+
+#FOR LOOP IN TUPLE
+nums = (1,2,3,4,5,6,7,8,9)
+for num in nums:
+    print(num)
+
+#CHARACTERS
+name = "sharath_kumar"
+for char in name:
+   print(char)
